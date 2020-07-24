1014fa0537ea1dff6d90621c71eb7b37c026ac4d
DaehoSon <thseogh135@gmail.com>
2020-07-24 22:50:18 +0900

    Day Nine&Ten Challenge

--- 09-10 day-nine-ten-challenge/app.py
+++ 09-10 day-nine-ten-challenge/app.py
@@ -1,33 +1,32 @@
+import requests
 from flask import Flask, render_template, request
-from get_datas import get_new_popular, get_comment
 
+base_url = "http://hn.algolia.com/api/v1"
+new_url = f"{base_url}/search_by_date?tags=story"
+popular_url = f"{base_url}/search?tags=story"
 
 db = {}
-comment_db = {}
-app = Flask("Day-Nine&Ten")
+app = Flask("DayNine")
 
 
 @app.route("/")
 def index():
-    order_by = request.args.get("order_by")
-    if not order_by:
-        order_by = "popular"
-
-    db_data = db.get(order_by)
-    if db_data:
-        news_datas = db_data
+    order_by = request.args.get("order_by", "popular")
+    # request.args에서 oder_by를 찾고 그 초기값을 popular로 정함
+    check_db = db.get(order_by)
+    if check_db:
+        story_datas = check_db
     else:
-        news_datas = get_new_popular(order_by)
-        db[order_by] = news_datas
-    return render_template("index.html", order_by=order_by, news_datas=news_datas)
+        if order_by == "popular":
+            story_datas = requests.get(popular_url).json().get("hits")
+        elif order_by == "new":
+            story_datas = requests.get(new_url).json().get("hits")
+        db[order_by] = story_datas
+    return render_template("index.html", story_datas=story_datas, order_by=order_by)
 
 
 @app.route("/<id>")
 def detail(id):
-    db_data = comment_db.get(id)
-    if db_data:
-        comment_data = db_data
-    else:
-        comment_data = get_comment(id)
-        comment_db[id] = comment_data
-    return render_template("detail.html", comment_data=comment_data)
+    detail_url = f"{base_url}/items/{id}"
+    detail_data = requests.get(detail_url).json()
+    return render_template("detail.html", detail_data=detail_data)
